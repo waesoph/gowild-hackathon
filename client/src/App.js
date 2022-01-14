1215799f8807bad63b7a6957810c41825139e846
Carol DeWeger <93411581+CDeweger@users.noreply.github.com>
2022-01-14 10:34:52 -0800

    Revert "2/feature/carol"

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import axios from "axios";
-
-import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
-import Select from "./pages/Select/Select";
-import Cart from "./pages/Cart/Cart";
-import Footer from "./components/Footer/Footer";
-import CustomizePage from "./pages/CustomizePage/CustomizePage";
-import CheckoutPage from "./pages/CustomizePage/CustomizePage";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import axios from "axios";
 
 import "./styles/global.scss";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/select" component={Select} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/customize" component={CustomizePage} />
-        <Route path="/checkout" component={CheckoutPage} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+    <div>Hi    
+    </div>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const productsRoute = require('./routes/products');
-const boxesRoute = require("./routes/boxes");
-
 //Config
 require("dotenv").config();
 const port = process.env.PORT;
@@ -12,9 +9,6 @@ const port = process.env.PORT;
 //Middleware
 app.use(express.json());
 app.use(cors());
-
-app.use('/products', productsRoute);
-app.use("/boxes", boxesRoute);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
